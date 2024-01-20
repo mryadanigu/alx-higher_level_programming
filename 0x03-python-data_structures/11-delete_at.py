851c295b5260e0589680d3a15cb1dfb5334400d4
mryadanigu <mryadanigu@gmail.com>
2024-01-20 12:32:58 +0300

    0x03-python-data_structures

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -1,18 +1,7 @@
 #!/usr/bin/python3
 
 def delete_at(my_list=[], idx=0):
-    # Check if the index is valid
-    if idx < 0 or idx >= len(my_list):
+    if idx < 0 or idx > (len(my_list) - 1):
         return my_list
-
-    # Delete the item at the specified index without using pop()
     new_list = my_list[:idx] + my_list[idx+1:]
-
     return new_list
-
-# Example usage
-my_list = [1, 2, 3, 4, 5]
-idx = 3
-new_list = delete_at(my_list, idx)
-print(new_list)
-print(my_list)
